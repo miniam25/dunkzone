3cccc99238bd79f845aea08c1f4333a012f52294
inliktor <milopafai@gmail.com>
2025-11-20 19:14:22 +0200

    Первый коммит начало инициализация main + layout

---
+++ app/src/main/java/com/example/dunkzone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dunkzone;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
